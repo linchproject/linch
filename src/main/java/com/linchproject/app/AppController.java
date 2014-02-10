16a4fafe6ceaca920635f0bd8157b6a2e5d132d4
Georg Schmidl <georg.schmidl@vicox.net>
2014-02-10 19:22:35 +0100

    first template rendering

--- src/main/java/com/linchproject/app/AppController.java
+++ src/main/java/com/linchproject/app/AppController.java
@@ -4,12 +4,18 @@ import com.linchproject.core.Controller;
 import com.linchproject.core.Params;
 import com.linchproject.core.Result;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Georg Schmidl
  */
 public class AppController extends Controller {
 
     public Result index(Params params) {
-        return success("Hello Linch");
+        Map<String, Object> context = new HashMap<String, Object>();
+        context.put("hello", "Hello Linch");
+
+        return render("index", context);
     }
 }
