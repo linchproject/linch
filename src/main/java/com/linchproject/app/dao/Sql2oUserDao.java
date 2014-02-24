fdbe7ac157a7efea493e73dc369d690c3cb6be20
Georg Schmidl <georg.schmidl@vicox.net>
2014-02-24 23:32:40 +0100

    added fullName

--- src/main/java/com/linchproject/app/dao/Sql2oUserDao.java
+++ src/main/java/com/linchproject/app/dao/Sql2oUserDao.java
@@ -35,13 +35,22 @@ public class Sql2oUserDao extends Sql2oDao implements UserDao, Component {
         if (model.getId() == null) {
             model.setId(query("insert into user ( username, password, first_name, last_name, email ) " +
                     "values ( :username, :password, :firstName, :lastName, :email )", true)
-                    .bind(model)
+                    .addParameter("username", model.getUsername()) //.bind(model) not possible because of fullName
+                    .addParameter("password", model.getPassword())
+                    .addParameter("firstName", model.getFirstName())
+                    .addParameter("lastName", model.getLastName())
+                    .addParameter("email", model.getEmail())
                     .executeUpdate()
                     .<Long>getKey(Long.class));
         } else {
             model.setId(query("update user set username = :username, password = :password, " +
                     "first_name = :firstName, last_name = :lastName, email = :email where id = :id", true)
-                    .bind(model)
+                    .addParameter("id", model.getId()) //.bind(model) not possible because of fullName
+                    .addParameter("username", model.getUsername())
+                    .addParameter("password", model.getPassword())
+                    .addParameter("firstName", model.getFirstName())
+                    .addParameter("lastName", model.getLastName())
+                    .addParameter("email", model.getEmail())
                     .executeUpdate()
                     .<Long>getKey(Long.class));
         }
