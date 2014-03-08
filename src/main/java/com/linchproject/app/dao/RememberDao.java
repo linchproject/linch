14ad352041a0c5b5b5ed2abec7be45dcfa86ed68
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-08 19:53:11 +0100

    implemented abstract dao from framework

--- src/main/java/com/linchproject/app/dao/RememberDao.java
+++ src/main/java/com/linchproject/app/dao/RememberDao.java
@@ -1,13 +1,75 @@
 package com.linchproject.app.dao;
 
 import com.linchproject.app.models.Remember;
+import com.linchproject.framework.db.Dao;
+import com.linchproject.ioc.Initializing;
+
+import java.util.List;
 
 /**
  * @author Georg Schmidl
  */
-public interface RememberDao extends Dao<Remember, Long> {
+public class RememberDao extends Dao<Remember> implements Initializing {
 
-    Remember findByUserId(Long userId);
+    @Override
+    public Remember findById(Long id) {
+        return query("select id, uuid, user_id from remember where id = :id")
+                .addParameter("id", id)
+                .executeAndFetchFirst(Remember.class);
+    }
 
-    Remember findByUuid(String uuid);
+    @Override
+    public List<Remember> findAll() {
+        return query("select id, uuid, user_id from remember")
+                .executeAndFetch(Remember.class);
+    }
+
+    @Override
+    public void save(Remember object) {
+        if (object.getId() == null) {
+            object.setId(query("insert into remember ( uuid, user_id ) " +
+                    "values ( :uuid, :userId )", true)
+                    .bind(object)
+                    .executeUpdate()
+                    .<Long>getKey(Long.class));
+        } else {
+            object.setId(query("update remember set uuid = :uuid, user_id = :userId,  where id = :id", true)
+                    .bind(object)
+                    .executeUpdate()
+                    .<Long>getKey(Long.class));
+        }
+    }
+
+    @Override
+    public void delete(Remember object) {
+        if (object.getId() != null) {
+            query("delete from remember where id = :id")
+                    .addParameter("id", object.getId())
+                    .executeUpdate();
+        }
+    }
+
+    public Remember findByUserId(Long userId) {
+        return query("select id, uuid, user_id from remember where user_id = :userId")
+                .addParameter("userId", userId)
+                .executeAndFetchFirst(Remember.class);
+    }
+
+    public Remember findByUuid(String uuid) {
+        return query("select id, uuid, user_id from remember where uuid = :uuid")
+                .addParameter("uuid", uuid)
+                .executeAndFetchFirst(Remember.class);
+    }
+
+    @Override
+    public void init() {
+        query("create table if not exists remember ( " +
+                "id int(11) unsigned not null auto_increment, " +
+                "uuid varchar(255) not null, " +
+                "user_id int(11) unsigned not null, " +
+                "primary key (id), " +
+                "key uuid (uuid), " +
+                "key user_id (user_id) " +
+                ") engine=InnoDB default charset=utf8").executeUpdate();
+    }
 }
--- src/main/java/com/linchproject/app/dao/UserDao.java
+++ src/main/java/com/linchproject/app/dao/UserDao.java
@@ -1,11 +1,91 @@
 package com.linchproject.app.dao;
 
 import com.linchproject.app.models.User;
+import com.linchproject.framework.db.Dao;
+import com.linchproject.ioc.Initializing;
+
+import java.util.List;
 
 /**
  * @author Georg Schmidl
  */
-public interface UserDao extends Dao<User, Long> {
+public class UserDao extends Dao<User> implements Initializing {
 
-    User findByUsername(String username);
+    public User findByUsername(String username) {
+        return query("select id, username, password, first_name, last_name, email from user where username = :username")
+                .addParameter("username", username)
+                .executeAndFetchFirst(User.class);
+    }
+
+    @Override
+    public User findById(Long id) {
+        return query("select id, username, password, first_name, last_name, email from user where id = :id")
+                .addParameter("id", id)
+                .executeAndFetchFirst(User.class);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return query("select id, username, password, first_name, last_name, email from user")
+                .executeAndFetch(User.class);
+    }
+
+    @Override
+    public void save(User object) {
+        if (object.getId() == null) {
+            object.setId(query("insert into user ( username, password, first_name, last_name, email ) " +
+                    "values ( :username, :password, :firstName, :lastName, :email )", true)
+                    .addParameter("username", object.getUsername()) //.bind(model) not possible because of fullName
+                    .addParameter("password", object.getPassword())
+                    .addParameter("firstName", object.getFirstName())
+                    .addParameter("lastName", object.getLastName())
+                    .addParameter("email", object.getEmail())
+                    .executeUpdate()
+                    .<Long>getKey(Long.class));
+        } else {
+            object.setId(query("update user set username = :username, password = :password, " +
+                    "first_name = :firstName, last_name = :lastName, email = :email where id = :id", true)
+                    .addParameter("id", object.getId()) //.bind(model) not possible because of fullName
+                    .addParameter("username", object.getUsername())
+                    .addParameter("password", object.getPassword())
+                    .addParameter("firstName", object.getFirstName())
+                    .addParameter("lastName", object.getLastName())
+                    .addParameter("email", object.getEmail())
+                    .executeUpdate()
+                    .<Long>getKey(Long.class));
+        }
+    }
+
+    @Override
+    public void delete(User object) {
+        if (object.getId() != null) {
+            query("delete from user where id = :id")
+                    .addParameter("id", object.getId())
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void init() {
+        query("create table if not exists user ( " +
+                "id int(11) unsigned not null auto_increment, " +
+                "username varchar(255) not null, " +
+                "password varchar(255) not null, " +
+                "first_name varchar(255), " +
+                "last_name varchar(255), " +
+                "email varchar(255), " +
+                "primary key (id), " +
+                "key username (username) " +
+                ") engine=InnoDB default charset=utf8").executeUpdate();
+
+        User user = query("select id from user").executeAndFetchFirst(User.class);
+
+        if (user == null) {
+            query("insert into user ( username, password ) " +
+                    "values ( :username, :password )")
+                    .addParameter("username", "admin")
+                    .addParameter("password", "N5lrX+ZfsB1MuC/J+frGztL/eRL0n+7J")
+                    .executeUpdate();
+        }
+    }
 }
