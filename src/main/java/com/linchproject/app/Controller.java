f4ca40a0c407145870187f883a77d94713b212a6
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-08 16:25:59 +0100

    added i18n to controller

--- src/main/java/com/linchproject/app/Controller.java
+++ src/main/java/com/linchproject/app/Controller.java
@@ -1,11 +1,15 @@
 package com.linchproject.app;
 
+import com.github.mustachejava.TemplateFunction;
 import com.linchproject.app.dao.RememberDao;
 import com.linchproject.app.dao.UserDao;
 import com.linchproject.app.models.Remember;
 import com.linchproject.app.models.User;
 import com.linchproject.framework.Context;
+import com.linchproject.framework.I18n;
+import com.linchproject.framework.components.I18nService;
 import com.linchproject.http.CookieService;
+import com.linchproject.http.LocaleService;
 import com.linchproject.http.SessionService;
 
 /**
@@ -15,12 +19,16 @@ public class Controller extends com.linchproject.framework.Controller {
 
     protected SessionService sessionService;
     protected CookieService cookieService;
+    protected LocaleService localeService;
+    protected I18nService i18nService;
 
     protected UserDao userDao;
     protected RememberDao rememberDao;
 
-    private User user;
     private boolean cookieChecked;
+
+    private User user;
+    private I18n i18n;
 
     protected User getUser() {
         String username = getUsernameFromSessionOrCookie();
@@ -29,6 +37,13 @@ public class Controller extends com.linchproject.framework.Controller {
             user = userDao.findByUsername(username);
         }
         return user;
+    }
+
+    protected I18n getI18n() {
+        if (i18n == null) {
+            i18n = i18nService.getI18n(localeService.getLocale());
+        }
+        return i18n;
     }
 
     private String getUsernameFromSessionOrCookie() {
@@ -54,7 +69,14 @@ public class Controller extends com.linchproject.framework.Controller {
 
     @Override
     protected Context context() {
-        return super.context().put("user", getUser());
+        return super.context()
+                .put("user", getUser())
+                .put("i18n", new TemplateFunction() {
+                    @Override
+                    public String apply(String input) {
+                        return getI18n().getText(input);
+                    }
+                });
     }
 
     public void setSessionService(SessionService sessionService) {
@@ -63,6 +85,14 @@ public class Controller extends com.linchproject.framework.Controller {
 
     public void setCookieService(CookieService cookieService) {
         this.cookieService = cookieService;
+    }
+
+    public void setLocaleService(LocaleService localeService) {
+        this.localeService = localeService;
+    }
+
+    public void setI18nService(I18nService i18nService) {
+        this.i18nService = i18nService;
     }
 
     public void setUserDao(UserDao userDao) {
