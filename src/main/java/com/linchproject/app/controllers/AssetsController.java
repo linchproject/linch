975a2e3a9e229fa772f99be6d5fc4d2b787bcdd9
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-11 19:42:58 +0100

    remove assetsController and implement _ method that call asset()

--- src/main/java/com/linchproject/app/controllers/AssetsController.java
+++ src/main/java/com/linchproject/app/controllers/AssetsController.java
@@ -1,8 +1,15 @@
 package com.linchproject.app.controllers;
 
+import com.linchproject.core.Params;
+import com.linchproject.core.Result;
+import com.linchproject.framework.Controller;
+
 /**
  * @author Georg Schmidl
  */
-public class AssetsController extends com.linchproject.framework.AssetsController {
+public class AssetsController extends Controller {
 
+    public Result _(Params params) {
+        return asset();
+    }
 }
