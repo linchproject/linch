44edee9b59c04259ef9767b39c3e9e04e20d8a50
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-16 16:40:36 +0100

    use form in users edit and add delete

--- src/main/java/com/linchproject/linch/controllers/admin/UsersController.java
+++ src/main/java/com/linchproject/linch/controllers/admin/UsersController.java
@@ -19,25 +19,6 @@ public class UsersController extends AdministratorController {
 
     protected PasswordEncryptor passwordEncryptor;
 
-    private Form createForm;
-
-    @Override
-    public void init() {
-        this.createForm = new I18nForm(getI18n())
-                .addField("username")
-                    .addValidator(new RequiredValidator())
-                    .addValidator(new UserExistsValidator())
-                .addField("firstName")
-                .addField("lastName")
-                .addField("email")
-                    .addValidator(new RequiredValidator())
-                    .addValidator(new EmailValidator())
-                .addField("password")
-                    .addValidator(new RequiredValidator())
-                .addField("confirmPassword")
-                    .addValidator(new EqualsValidator("password"))
-                .form();
-    }
 
     public Result index(Params params) {
         return render("admin/users/index", context()
@@ -56,19 +37,40 @@ public class UsersController extends AdministratorController {
         Result result;
         User user = userDao.findByUsername(params.get("username"));
 
+        Form form = new I18nForm(getI18n())
+                .addField("username")
+                .addField("firstName")
+                .addField("lastName")
+                .addField("email")
+                    .addValidator(new RequiredValidator())
+                    .addValidator(new EmailValidator())
+                .form();
+
         if (params.get("submit") != null) {
-            user.setFirstName(params.get("firstName"));
-            user.setLastName(params.get("lastName"));
-            if (params.get("email") != null) {
-                user.setEmail(params.get("email"));
+            form.fill(params.getMap()).validate();
+
+            if (form.isValid()) {
+                user.setUsername(form.get("username").get());
+                user.setFirstName(form.get("firstName").get());
+                user.setLastName(form.get("lastName").get());
+                user.setEmail(form.get("email").get());
+                userDao.save(user);
+
+                result = redirect("view?username=" + user.getUsername());
+            } else {
+                result = render("admin/users/edit", context()
+                        .put("navUsers", true)
+                        .put("form", form));
             }
-            userDao.save(user);
-            result = redirect("view?username=" + user.getUsername());
 
         } else {
             result = render("admin/users/edit", context()
                     .put("navUsers", true)
-                    .put("theUser", user));
+                    .put("form", form
+                            .put("username", user.getUsername())
+                            .put("firstName", user.getFirstName())
+                            .put("lastName", user.getLastName())
+                            .put("email", user.getEmail())));
         }
         return result;
     }
@@ -76,29 +78,60 @@ public class UsersController extends AdministratorController {
     public Result create(Params params) {
         Result result;
 
-        if (params.get("submit") != null) {
-            createForm.fill(params.getMap()).validate();
+        Form form = new I18nForm(getI18n())
+                .addField("username")
+                    .addValidator(new RequiredValidator())
+                    .addValidator(new UserExistsValidator())
+                .addField("firstName")
+                .addField("lastName")
+                .addField("email")
+                    .addValidator(new RequiredValidator())
+                    .addValidator(new EmailValidator())
+                .addField("password")
+                    .addValidator(new RequiredValidator())
+                .addField("confirmPassword")
+                    .addValidator(new EqualsValidator("password"))
+                .form();
 
-            if (createForm.isValid()) {
+        if (params.get("submit") != null) {
+            form.fill(params.getMap()).validate();
+
+            if (form.isValid()) {
                 User user = new User();
-                user.setUsername(createForm.get("username").get());
-                user.setFirstName(createForm.get("firstName").get());
-                user.setLastName(createForm.get("lastName").get());
-                user.setEmail(createForm.get("email").get());
-                user.setPassword(passwordEncryptor.encryptPassword(createForm.get("password").get()));
+                user.setUsername(form.get("username").get());
+                user.setFirstName(form.get("firstName").get());
+                user.setLastName(form.get("lastName").get());
+                user.setEmail(form.get("email").get());
+                user.setPassword(passwordEncryptor.encryptPassword(form.get("password").get()));
                 userDao.save(user);
 
                 result = redirect("view?username=" + user.getUsername());
             } else {
                 result = render("admin/users/create", context()
                         .put("navUsers", true)
-                        .put("form", createForm));
+                        .put("form", form));
             }
 
         } else {
             result = render("admin/users/create", context()
                     .put("navUsers", true)
-                    .put("form", createForm));
+                    .put("form", form));
+        }
+        return result;
+    }
+
+    public Result delete(Params params) {
+        Result result;
+        User user = userDao.findByUsername(params.get("username"));
+
+        if (params.get("submit") != null) {
+            userDao.delete(user);
+            result = redirect("index");
+
+        } else {
+            result = render("admin/users/delete", context()
+                    .put("navUsers", true)
+                    .put("theUser", user));
         }
         return result;
     }
