74a614a6cd0c8f0ac69f63b316fe6022a685cea7
Georg Schmidl <georg.schmidl@vicox.net>
2014-03-30 22:08:56 +0200

    set controller package if path is used by an app

--- src/main/java/com/linchproject/linch/controllers/AppController.java
+++ src/main/java/com/linchproject/linch/controllers/AppController.java
@@ -2,9 +2,12 @@ package com.linchproject.linch.controllers;
 
 import com.linchproject.core.Params;
 import com.linchproject.core.Result;
+import com.linchproject.core.Route;
+import com.linchproject.linch.App;
 import com.linchproject.linch.Controller;
 import com.linchproject.linch.dao.SettingDao;
 import com.linchproject.linch.entities.Setting;
+import com.linchproject.linch.services.AppService;
 
 /**
  * @author Georg Schmidl
@@ -12,11 +15,22 @@ import com.linchproject.linch.entities.Setting;
 public class AppController extends Controller {
 
     protected SettingDao settingDao;
+    protected AppService appService;
 
     public Result index(Params params) {
-        Setting indexPath = settingDao.findByKey("indexPath");
-        if (indexPath != null) {
-            return dispatch(indexPath.getValue());
+        Setting indexPathSetting = settingDao.findByKey("indexPath");
+        if (indexPathSetting != null) {
+            String indexPath = indexPathSetting.getValue();
+            Route route = this.route.copy();
+
+            App app = appService.getApp(indexPath);
+            if (app != null) {
+                route.setControllerPackage(app.getAppPackage() + ".controllers");
+            } else {
+                route.setPath(indexPath);
+            }
+
+            return dispatch(route);
         }
 
         return render(context().put("hello", "Hello Linch"));
@@ -24,5 +38,9 @@ public class AppController extends Controller {
 
     public void setSettingDao(SettingDao settingDao) {
         this.settingDao = settingDao;
+    }
+
+    public void setAppService(AppService appService) {
+        this.appService = appService;
     }
 }
